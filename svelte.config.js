81c09b0c491a69eb4979a8a33726f7913565ed95
Thariq Aulia Akbar <thariq2712@gmail.com>
2025-12-28 15:58:27 +0700

    add svelte config for ssg github-pages

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,17 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: { 
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}},
 };
 
 export default config;
